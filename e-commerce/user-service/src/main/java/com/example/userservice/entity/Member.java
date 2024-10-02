d8d93efd1dbd946e2aad0ec941544c549b43680d
yn15 <yn15@naver.com>
2024-10-02 17:48:05 +0900

    COMMIT: Users Microservice - Init
    * Config용 Greeting class 생성 및 Value 추가
    * h2 Database 연동 및 Error해결을 위한 jpa dependency 추가 및 Entity를 이용한 데이터베이스 생성

---
+++ e-commerce/user-service/src/main/java/com/example/userservice/entity/Member.java
@@ -0,0 +1,14 @@
+package com.example.userservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+}
