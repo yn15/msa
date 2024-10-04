45df593ce9f991e0331fbe83d2aff1327abd26e3
yn15 <yn15@naver.com>
2024-10-04 12:13:04 +0900

    COMMIT: Users Microservice - Spring Security 연동 Spring 3 버전 대 이상으로 Websecurity chain으로 변경 /users/**, h2-console permitAll

---
+++ e-commerce/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,33 @@
+package com.example.userservice.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    private static final String[] WHITE_LIST = {
+            "/users/**",
+            "/**"
+    };
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+
+        http.csrf((csrfConfig) -> csrfConfig.disable()
+        ).headers((headersConfig) -> headersConfig.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/users/**").permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                );
+
+
+        return http.build();
+    }
+
+}
