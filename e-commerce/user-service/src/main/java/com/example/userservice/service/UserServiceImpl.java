10d2689e28d2787fec2d4f3d4f2391a30825d326
yn15 <yn15@naver.com>
2024-10-08 17:42:39 +0900

    COMMIT: Users Microservice - MSA 통신 * Feign Exception

--- e-commerce/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ e-commerce/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.userservice.dto.UserDTO;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
     UserRepository userRepository;
     BCryptPasswordEncoder passwordEncoder;
@@ -80,8 +83,14 @@ public class UserServiceImpl implements UserService {
 //        List<ResponseOrder> ordersList = orderListResponse.getBody();
 
         /* Using a feign client */
+        /* Feign exception handling */
+        List<ResponseOrder> ordersList = null;
+        try{
+            ordersList = orderServiceClient.getOrders(userId);
+        }catch (FeignException ex){
+            log.error(ex.getMessage());
+        }
 
-        List<ResponseOrder> ordersList = orderServiceClient.getOrders(userId);
 
         userDTO.setOrders(ordersList);
 
